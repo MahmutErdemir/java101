45d8d1a693d9ebdb1c89e540f80eb7261c64b5d2
MAHMUT <113979137+ICONICMEMBER@users.noreply.github.com>
2022-12-07 14:07:08 +0300

    www.patika.dev

---
+++ Manav_Kasa_Programi/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double armut=2.14,elma=3.67,domates=1.11,muz=0.95,patlican=5;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Armut Kaç Kilo ? :");
+        armut*=input.nextDouble();
+        System.out.print("Elma Kaç Kilo ? :");
+        elma*=input.nextDouble();
+        System.out.print("Domates Kaç Kilo ? :");
+        domates*=input.nextDouble();
+        System.out.print("Muz Kaç Kilo ? :");
+        muz*=input.nextDouble();
+        System.out.print("Patlıcan Kaç Kilo ? :");
+        patlican*=input.nextDouble();
+        System.out.print("Toplam Tutar :" +(armut+elma+domates+muz+patlican) +" TL");
+
+
+
+
+    }
+}
