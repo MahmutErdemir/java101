7dfc355c2395e099c34e98b8105246c9bb4699c8
MAHMUT <113979137+ICONICMEMBER@users.noreply.github.com>
2022-12-07 10:33:39 +0300

    www.patika.dev
    www.patika.dev

--- KDV Tutari Hesaplayan Program/src/Main.java
+++ KDV Tutari Hesaplayan Program/src/Main.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double tutar,kdvOran=0.18,kdvTutar,kdvliTutar;
+        double tutar,kdvOran1=0.18,kdvOran2=0.08,kdvOran,kdvTutar,kdvliTutar;
+
         Scanner input = new Scanner(System.in);
         System.out.print("Ücret tutarını giriniz: ");
         tutar =input.nextDouble();
-        kdvTutar = tutar*kdvOran;
+
+
+        double str =(1000>=tutar  && tutar>=0) ? (kdvOran=kdvOran1) :(kdvOran=kdvOran2);
+        kdvTutar=kdvOran*tutar;
         kdvliTutar=tutar+kdvTutar;
         System.out.println("KDV'siz Tutar :"+ tutar);
         System.out.println("KDV Oranı :"+ kdvOran);
