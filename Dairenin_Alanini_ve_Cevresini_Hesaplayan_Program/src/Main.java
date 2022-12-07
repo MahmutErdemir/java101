0e152e8facfcc262997e761103410ddb9117ce7a
MAHMUT <113979137+ICONICMEMBER@users.noreply.github.com>
2022-12-07 12:08:03 +0300

    www.patika.dev

--- Dairenin_Alanini_ve_Cevresini_Hesaplayan_Program/src/Main.java
+++ Dairenin_Alanini_ve_Cevresini_Hesaplayan_Program/src/Main.java
@@ -4,14 +4,19 @@ public class Main {
     public static void main(String[] args) {
 
         int r;
-        double pi=3.14,alan,cevre;
+        double pi=3.14,alan,cevre,angle,alan2;
         Scanner input = new Scanner(System.in);
         System.out.print("Dairenin yarı çapını giriniz: ");
         r=input.nextInt();
+        System.out.print("Dairenin Diliminin Açısını Giriniz: ");
+        angle=input.nextInt();
         alan =pi*r*r;
         cevre=2*pi*r;
-        System.out.print("Dairenin alanı: " + alan);
-        System.out.print("Dairenin çevresi: " + cevre);
+        alan2=(pi * (r*r) * angle) / 360;
+        System.out.println("Dairenin alanı: " + alan);
+        System.out.println("Dairenin çevresi: " + cevre);
+        System.out.println("Dairenin Diliminin Alanı: " + alan2);
+
 
 
     }
